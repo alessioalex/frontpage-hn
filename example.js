d846a2dba7c5f928eadf9ca1bf1d06eaa8901950
Alexandru Vlăduţu <github@social.alessioalex.com>
2021-11-11 15:07:05 +0200

    wip - new API

--- example.js
+++ example.js
@@ -1,20 +1,55 @@
 /* eslint-disable no-console, func-names */
 'use strict';
 
-var getFrontPageHN = require('./');
+const { getHNPosts, getHNPostsAsPromised } = require('./');
 
-getFrontPageHN(function(err, posts) {
+// example 1: standard HN front page
+// comment/uncomment the following line
+/*
+getHNPosts(function(err, posts) {
   if (err) { throw err; }
 
   console.log(posts);
 });
+// */
+
+// example 2: order by 'score' descending (and using options)
+// (defaults to regular HN position, can also be set to 'comments')
+// 
+// comment/uncomment the following line
+/*
+getHNPosts({ orderBy: 'score' }, function(err, posts) {
+  if (err) { throw err; }
+
+  console.log(posts);
+});
+// */
+
+// example 3: using async await with Promises instead of callbacks
+// 
+// comment/uncomment the following line
+// /*
+(async function() {
+  try {
+    const posts = await getHNPostsAsPromised({ orderBy: 'comments' });
+    console.log(posts);
+  } catch (err) {
+    console.error('oh noes, something bad happened while getting the HN posts :/')
+    throw err;
+  }
+}());
+// */
+
 
 // Example output:
+
+// 30 items that look like the following ->
 /*
   [{
     title: 'Deep Learning: An MIT Press Book in Preparation',
     postLink: 'http://goodfeli.github.io/dlbook/',
     commentsLink: 'https://news.ycombinator.com/item?id=10768440',
+    score: 465,
     comments: 20
   },
   ...
--- index.js
+++ index.js
@@ -1,63 +1,121 @@
 'use strict';
 
-var get = require('simple-get').concat;
-var cheerio = require('cheerio');
-var errTo = require('errto');
-var Err = require('custom-err');
+const get = require('simple-get').concat;
+const cheerio = require('cheerio');
 
-function getFrontPageHN(cb) {
+// custom field can be one of 'score' or 'comments'
+// it defaults to 'position' in which case it doesn't
+// enter this function
+function orderBy(news, field) {
+  const secondaryField = field === 'score' ? 'comments' : 'score';
+
+  return news.sort(function(a, b) {
+    if (a[field] > b[field]) { return -1; }
+    if (a[field] < b[field]) { return 1; }
+
+    if (a[secondaryField] > b[secondaryField]) { return -1; }
+    if (a[secondaryField] < b[secondaryField]) { return 1; }
+
+    return 0;
+  });
+}
+
+function getHNPosts(options, cb) {
+  let done, opts;
+
+  if (typeof options === 'function') {
+    done = options;
+    opts = {};
+  } else {
+    opts = options;
+    done = cb;
+  }
+
+  if (opts && ('orderBy' in opts) && ['score', 'comments'].includes(opts.orderBy) === false) {
+    throw new Error("orderBy property can only be one of 'score' or 'comments', if you don't specify it then it defaults to 'position'");
+  }
+
+  const hnUrl = 'https://news.ycombinator.com/';
+
   get({
-    url: 'https://news.ycombinator.com/',
+    url: hnUrl,
     headers: {
       Accept: '*/*',
       Connection: 'close',
       'User-Agent': 'frontpage-hn'
     }
-  }, errTo(cb, function handleResponse(data, res) {
-    if (res.statusCode !== 200) {
-      cb(Err('HN request returned bad status code', {
-        code: res.statusCode,
-        responseBody: data,
-        responseHeaders: res.headers
-      }));
-    } else {
-      var news = [];
-      var $ = cheerio.load(data.toString('utf8'));
-
-      $('.title > a').each(function parseData() {
-        var el = $(this);
-        var title = el.text();
-
-        // last link is 'More' so we're not interested
-        if (!/^More$/i.test(title)) {
-          var commentsEl = el.parents('.athing').next().find('.subtext a').last();
-          var commentsLink = commentsEl.attr('href');
-
-          // if it doesn't have a comments link it's not of value (hiring posts etc)
-          if (!commentsLink || /^hide/.test(commentsLink)) { return; }
-
-          var tmp = commentsLink.match(/item\?id=(.*)/);
-          var postId = 0;
-
-          if (tmp && tmp[1]) {
-            postId = parseInt(tmp[1], 10);
-          } else {
-            throw new Error('Cannot get postId for ' + title + ': ' + commentsLink);
-          }
-
-          news.push({
-            id: postId,
-            title: title,
-            postLink: el.attr('href'),
-            commentsLink: 'https://news.ycombinator.com/' + commentsLink,
-            comments: parseInt(commentsEl.text(), 10) || 0
-          });
-        }
-      });
-
-      cb(null, news);
+  }, function handleResponse(err, data, res) {
+    if (err) {
+      const e = new Error(`Could not retrieve HN posts - GET ${hnUrl}`);
+      // https://github.com/tc39/proposal-error-cause
+      e.cause = err;
+      return done(e);
     }
-  }));
+
+    if (res.statusCode !== 200) {
+      const error = new Error(`Received HTTP code ${res.statusCode} for GET ${hnUrl}`);
+      error.statusCode = res.statusCode;
+      error.headers = res.headers;
+      error.body = res.body;
+
+      return done(error);
+    }
+
+    let news = [];
+    const $ = cheerio.load(data.toString('utf8'));
+
+    $('.title > a').each(function parseData() {
+      const el = $(this);
+      const title = el.text();
+
+      // last link is 'More' so we're not interested
+      if (!/^More$/i.test(title)) {
+        const scoreAndCommentsEl = el.parents('.athing').next().find('.subtext');
+        const score = parseInt(scoreAndCommentsEl.find('.score').text(), 10);
+        const commentsEl = scoreAndCommentsEl.find('a').last();
+        const commentsLink = commentsEl.attr('href');
+
+        // if it doesn't have a comments link it's not of value (hiring posts etc)
+        if (!commentsLink || /^hide/.test(commentsLink)) { return; }
+
+        const tmp = commentsLink.match(/item\?id=(.*)/);
+
+        const id = (tmp && tmp[1]) ? parseInt(tmp[1], 10) : 0;
+
+        if (id === 0) {
+          throw new Error('Cannot get postId for ' + title + ': ' + commentsLink);
+        }
+
+        news.push({
+          id,
+          score,
+          title,
+          postLink: el.attr('href'),
+          commentsLink: `https://news.ycombinator.com/${commentsLink}`,
+          comments: parseInt(commentsEl.text(), 10) || 0
+        });
+      }
+    });
+
+    news = opts.orderBy ? orderBy(news, opts.orderBy) : news;
+
+    done(null, news);
+  });
 }
 
-module.exports = getFrontPageHN;
+function getHNPostsAsPromised(opts = {}) {
+  return new Promise(function getPostsAsPromised(resolve, reject) {
+    getHNPosts(opts, function hnPostsCb(err, posts) {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve(posts);
+    });
+  });
+}
+
+module.exports = {
+  getHNPosts,
+  getHNPostsAsPromised
+};
